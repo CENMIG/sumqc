65f997ff84f852eb439fcaac5534384dd47e15ea
thavin <thavin.bod@gmail.com>
2021-07-15 23:04:35 +0700

    Add getopts in extract_fastqc() which incluing -b for print basename instead of filename in FileName column -o for omit entire FileName column Also removing PoorFlag column out of the extraction process.

--- awk.sh
+++ awk.sh
@@ -1,17 +1,51 @@
 extract_fastqc () {
-    echo -e "FileName\tTotalSeq\tPoorQualflag\tLength\tGC\tAvgQScore (min,max)";
+    if [ -z $1  ];
+    then
+        echo "Supplies one or more fastqc result in zip file."
+    fi
+    unset FileName BaseNameSignal OmitSignal OPTIND
+    FileName="FileName\t"
+    while getopts "bo" opt;
+    do
+        case $opt in
+            #Print filename as in basename
+            b) BaseNameSignal='1';;
+
+            #Omit filename column
+            o) OmitSignal='1'
+               FileName=""
+               ;;
+        esac
+    done
+    shift "$(( OPTIND - 1 ))"
+    echo $1
+    #Throw exception if both arguments are called
+    if [[ -n $BaseName ]] && [[ -n $OmitSignal ]]; then
+       echo "only pick -b or -o arguments"
+       exit
+    fi
+
+    echo -e "${FileName}TotalSeq\tLength\tGC\tAvgQScore (min,max)";
     for file in $@
     do
-        unzip -p $file ${file%.*}/fastqc_data.txt | \
-        awk -F '\t' '/Filename/{file=$2}
+        trimfile=${file##*/}
+        unzip -p $file ${trimfile%.*}/fastqc_data.txt | \
+        awk -F '\t'  -v omit=$OmitSignal -v base=$BaseNameSignal -v name=${trimfile%.*} \
+        '/Filename/{if ( length(base)==0 ) {file=$2} else {gsub(/_[12].*/,"");file=$2}}
         /Total Sequences/{seq=$2}
-        /Sequences flagged as poor quality/{flg=$2}
         /Sequence length/{len=$2}
         /%GC/{gc=$2}
         /Per sequence quality scores/,/END/{nr[NR]=$1; n++ ;nrr[n]=$1 ; min=nrr[3]; max=nr[NR-1] ;$3=$1*$2; sumn+=$2;sum+=$3 ;}
-        END{printf "%s\t%s\t%s\t%s\t%.3f\t%.3f (%s,%s)\n",file,seq,flg,len,gc,sum/sumn,min,max}';
+    END{if (length(omit)==0){
+        if ( sumn==0 ) {printf "%s\t%s\t%s\t%.3f\t%.3f (%s,%s)\n",file,0,0,0,0,0,0 ; exit};
+        printf "%s\t%s\t%s\t%.3f\t%.3f (%s,%s)\n",file,seq,len,gc,sum/sumn,min,max
+         }
+    else {
+        if ( sumn==0 ) {printf "%s\t%s\t%.3f\t%.3f (%s,%s)\n",0,0,0,0,0,0 ; exit};
+        printf "%s\t%s\t%.3f\t%.3f (%s,%s)\n",seq,len,gc,sum/sumn,min,max
+         }}' OFS='\t'
     done
-    }
+}
 
 calculate_percent () {
     #hard code col number $2-raw $8-pair $14-unpair $19-droppped (add)
